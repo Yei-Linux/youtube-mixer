f66cd5eb2f934eba3f1ec7c27e6f60fe25dedb53
Cesar Jesus Alberto Alvan Huarcaya <jesusalvan2010@gmail.com>
2023-09-02 00:22:44 -0500

    feat: Complete algorithm of JSON parser from strings and complexity :sparkles:

--- back/src/helpers/parse.ts
+++ back/src/helpers/parse.ts
@@ -19,17 +19,23 @@ export const parseYTSourceText = (text: string, ytSearchData: string[]) => {
 
   const [, right] = text.split(searchMatch);
 
-  const ytInitialDataArr = right.trim().split('');
+  const ytInitialDataArr = right.trim().replace(/\n/g, '').split('');
 
   const jsonParsed = validateEachCharacter(ytInitialDataArr);
 
   try {
     return JSON.parse(jsonParsed);
   } catch (error) {
+    console.log('test', jsonParsed);
     throw new Error('Error parsing json string');
   }
 };
 
+/**
+ * @summary Loop each character and extract only the object part from string
+ * @param ytInitialDataArr
+ * @returns
+ */
 export const validateEachCharacter = (ytInitialDataArr: string[]) => {
   let iterator = 0;
   let jsonCollected = '';
@@ -38,20 +44,24 @@ export const validateEachCharacter = (ytInitialDataArr: string[]) => {
     let matcherChar = starterChar;
     let jumperChar = starterChar === '{' ? '[' : '{';
 
+    let isInString = undefined;
     let counterCharValidator = 0;
     let matchers = charValidators[matcherChar];
     let jumpers = charValidators[jumperChar];
 
     while (iterator < ytInitialDataArr.length) {
       const char = ytInitialDataArr[iterator];
+      const nextChar = ytInitialDataArr[iterator + 1];
+      const nextCharIsAValidCloseString =
+        [':', '}', ']', ',', ' '].includes(nextChar) && char === '"';
 
-      const matchWithJumperChar = jumpers[0] === char;
+      const matchWithJumperChar = jumpers[0] === char && !isInString;
       if (matchWithJumperChar) {
         recursion(jumperChar);
         continue;
       }
 
-      const matchWithValidChar = matchers.includes(char);
+      const matchWithValidChar = matchers.includes(char) && !isInString;
       if (matchWithValidChar && char === matchers[0]) {
         counterCharValidator++;
       }
@@ -59,7 +69,19 @@ export const validateEachCharacter = (ytInitialDataArr: string[]) => {
         counterCharValidator--;
       }
 
-      jsonCollected += char;
+      const isInsideString =
+        char === '"' && isInString && !nextCharIsAValidCloseString;
+      jsonCollected += isInsideString ? `'` : char;
+
+      const isInStringModified: boolean = !!(char === '"'
+        ? !isInString
+          ? true
+          : nextCharIsAValidCloseString
+          ? false
+          : isInString
+        : isInString);
+      isInString = isInStringModified;
+
       iterator++;
       if (counterCharValidator === 0) {
         return jsonCollected;
--- back/tests/helpers/parse-yt-source.test.ts
+++ back/tests/helpers/parse-yt-source.test.ts
@@ -2,19 +2,51 @@ import 'mocha';
 import { parseYTSourceText } from '../../src/helpers/parse';
 import {
   mock_parse_yt_source_basic_case,
-  mock_parse_yt_source_case1,
-  mock_parse_yt_source_case2,
+  mock_parse_yt_source_basic_case_2,
+  mock_parse_yt_strange_characters,
+  mock_parse_yt_real_playlist_html,
+  mock_parse_yt_one_part,
 } from '../../src/mocks/parse-yt-source.mock';
 
-describe('Parsing youtube content', () => {
-  it('When is got a large string from youtube', () => {
-    const parsed = parseYTSourceText(mock_parse_yt_source_case2, [
-      'var ytInitialData=',
-      'var ytInitialData =',
-      'window["ytInitialData"]=',
-      'window["ytInitialData"] =',
-    ]);
+const splitterOne = [
+  'var ytInitialData=',
+  'var ytInitialData =',
+  'window["ytInitialData"]=',
+  'window["ytInitialData"] =',
+];
 
-    console.log('test', parsed);
+describe('Parsing youtube content', () => {
+  it('When is got a large string from case_one', () => {
+    const parsed = parseYTSourceText(
+      mock_parse_yt_source_basic_case,
+      splitterOne
+    );
+  });
+  it('When is got a large string from case_two', () => {
+    const parsed = parseYTSourceText(
+      mock_parse_yt_source_basic_case_2,
+      splitterOne
+    );
+  });
+  it('When is got a large string from strange characters case', () => {
+    const parsed = parseYTSourceText(
+      mock_parse_yt_strange_characters,
+      splitterOne
+    );
+  });
+  it('When is got a large string from youtube one part', () => {
+    const parsed = parseYTSourceText(mock_parse_yt_one_part, splitterOne);
+  });
+
+  it('When is got a large string from youtube', () => {
+    const parsed = parseYTSourceText(
+      mock_parse_yt_real_playlist_html,
+      splitterOne
+    );
+
+    const playList =
+      parsed.contents.twoColumnWatchNextResults.playlist.playlist;
+
+    console.log('test', playList);
   });
 });
